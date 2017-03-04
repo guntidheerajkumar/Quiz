d86d37cb0f013ffeeb145aef47d91281b23467b1
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2017-03-04 21:31:11 +0530

    Initial Project Setup

---
+++ Quiz/Quiz/Main.cs
@@ -0,0 +1,22 @@
+﻿//
+// Main.cs
+//
+// Author: Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
+//
+// Copyright (c) 2017 (c) Dheeraj Kumar Gunti
+//
+using UIKit;
+
+namespace Quiz
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	}
+}
---
+++ Quiz/Quiz/ViewController.cs
@@ -0,0 +1,33 @@
+﻿//
+// ViewController.cs
+//
+// Author: Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
+//
+// Copyright (c) 2017 (c) Dheeraj Kumar Gunti
+//
+using System;
+
+using UIKit;
+
+namespace Quiz
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
