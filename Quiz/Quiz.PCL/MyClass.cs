484cbdba0f73546bc6cf32dba038c01261e3230c
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2017-03-07 11:12:35 +0530

    Modified.

---
+++ Quiz/Quiz.PCL/MyClass.cs
@@ -0,0 +1,17 @@
+﻿//
+// MyClass.cs
+//
+// Author: Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
+//
+// Copyright (c) 2017 (c) Dheeraj Kumar Gunti
+//
+using System;
+namespace Quiz.PCL
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
